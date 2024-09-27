34bf380279b34e6fadf19b03b2bbe4b62c9d5aa1
Jared Newnam <jnewnam@scottsdaleaz.gov>
2024-09-26 17:24:01 -0700

    added install file

--- js/app.js
+++ js/app.js
@@ -448,13 +448,41 @@ document.addEventListener("DOMContentLoaded", function () {
 	displayTasks();
 });
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-      console.log('Service Worker registered with scope: ', registration.scope);
-    }, function(error) {
-      console.log('Service Worker registration failed: ', error);
-    });
-  });
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", function () {
+		navigator.serviceWorker.register("/service-worker.js").then(
+			function (registration) {
+				console.log("Service Worker registered with scope: ", registration.scope);
+			},
+			function (error) {
+				console.log("Service Worker registration failed: ", error);
+			}
+		);
+	});
 }
 
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+	e.preventDefault();
+	deferredPrompt = e;
+	const installButton = document.getElementById("installButton");
+	installButton.style.display = "block";
+
+	installButton.addEventListener("click", () => {
+		installButton.style.display = "none";
+		deferredPrompt.prompt();
+		deferredPrompt.userChoice.then((choiceResult) => {
+			if (choiceResult.outcome === "accepted") {
+				console.log("User accepted the install prompt");
+			} else {
+				console.log("User dismissed the install prompt");
+			}
+			deferredPrompt = null;
+		});
+	});
+});
+
+window.addEventListener("appinstalled", (evt) => {
+	console.log("PWA was installed successfully");
+});
